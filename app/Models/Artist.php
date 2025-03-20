f998f70a21ce3048f950e6f86d155a6ce7e70de4
Kenza ZEGGAF EL GALB <0911kezeggaf@student.epfc.eu>
2025-03-20 16:48:51 +0100

    Model Artist + migration

---
+++ app/Models/Artist.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    protected $fillable = ['firstname', 'lastname'];
+    protected $table = 'artists';
+    public $timestamps = false;
+
+}
